be386cf6923943be9d6c1a89383cc77cacda3b09
jacek444466 <jackuhebda@gmail.com>
2016-02-12 20:47:25 +0100

    prawie skoncone pierwsze activity

--- app/src/main/java/com/example/jacek/jakinapiwek/MainActivity.java
+++ app/src/main/java/com/example/jacek/jakinapiwek/MainActivity.java
@@ -6,15 +6,25 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity  {
     Button oblicz;
     Button plusjeden;
+    EditText ileosob;
+    EditText zaplata;
+    int iloscOsob=0;
+    String iloscOsobString="1";
+    int ileOsobFinal=0;
+    double doZaplaty=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +32,40 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         oblicz=(Button)findViewById(R.id.oblicz);
         plusjeden=(Button)findViewById(R.id.plusjednbutton);
-        oblicz.setOnClickListener(this);
-        plusjeden.setOnClickListener(this);
+        ileosob=(EditText)findViewById(R.id.ileosob);
+        zaplata=(EditText)findViewById(R.id.zaplata);
+
+        //plusjeden.setOnClickListener(this);
+
+        View.OnClickListener obliczenia =new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ileOsobString=ileosob.getText().toString();
+                if(TextUtils.isEmpty(ileOsobString)) {
+                    ileosob.setError("Podaj liczbę osób");
+                    return;
+                }
+                Log.d("aa","null");
+                // int ileOsobFinal=Integer.parseInt(ileosob.getText().toString());
+               // String cena=zaplata.getText().toString();
+                //double kwota=Double.parseDouble(zaplata.getText().toString());
+                //doZaplaty=kwota+(kwota*0.1);
+               // String ile=Double.toString(doZaplaty);
+              Log.d("aa","11");
+            }
+        };
+        oblicz.setOnClickListener(obliczenia);
+        View.OnClickListener dodaj = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iloscOsob+=1;
+                iloscOsobString=Integer.toString(iloscOsob);
+                ileosob.setText(iloscOsobString);
+                ileosob.setError(null);
+            }
+        };
+        plusjeden.setOnClickListener(dodaj);
+
 
 
 
@@ -51,8 +93,5 @@ public class MainActivity extends Activity implements View.OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View v) {
 
-    }
 }
