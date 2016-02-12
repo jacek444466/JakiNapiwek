ac5332458a0cb64878d48657e17e7d6e5c4bb3d7
jacek444466 <jackuhebda@gmail.com>
2016-02-12 14:53:11 +0100

    gotowy lauout

--- app/src/main/java/com/example/jacek/jakinapiwek/MainActivity.java
+++ app/src/main/java/com/example/jacek/jakinapiwek/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.jacek.jakinapiwek;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    Button oblicz;
+    Button plusjeden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        oblicz=(Button)findViewById(R.id.oblicz);
+        plusjeden=(Button)findViewById(R.id.plusjednbutton);
+        oblicz.setOnClickListener(this);
+        plusjeden.setOnClickListener(this);
+
 
 
     }
@@ -39,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
